b8dde09ecee8d786036beee4c02a1d0da0d4d806
Taykahn <marshall962000@yahoo.com>
2016-12-19 22:38:08 -0600

    Add files via upload

---
+++ mock-website/footer.php
@@ -0,0 +1,19 @@
+</div><!-- end content -->
+
+	<div class="footer">
+
+		<div class="wrapper">
+
+			<ul>		
+				<li><a href="http://twitter.com/mcarter">Twitter</a></li>
+				<li><a href="https://www.facebook.com/marshallcarter">Facebook</a></li>
+			</ul>
+
+			<p>&copy;<?php echo date("Y"); ?></p>
+
+		</div>
+	
+	</div>
+
+</body>
+</html>
---
+++ mock-website/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>Pappy's Grill and Bar</title>
+	</head>
+	<body>
+		<header>
+				<h1>Pappy's Grill and Bar</h1>
+				<h2>St. Joseph, Mo.</h2>
+			<nav>
+				<ul>
+					<li><a href="home.html">Home</a></li>
+					<li><a href="about.html">About</a></li>
+					<li><a href="events.html">Events</a></li>
+					<li><a href="photos.html">Photos</a></li>
+					<li><a href="videos.html">Videos</a></li>
+					<li><a href="guidoscolumn.html">Guido's Column</a></li>
+					<li><a href="contact.html">Contact</a></li>
+				</ul>
+				<div>
+					<img src="facade5mockwebsite.png" alt="Pappy's Store Front">
+					<div>Welcome to Pappy's</div>
+					<div>
+						<p>We hope you enjoy Pappy's Web Site. We've made it easy for you to view our <a href="menu.html">menu</a>, check out our upcoming <a href="events.html">events</a> and enjoy the fun times at Pappy's in our <a href="photos.html">photos</a> and <a href="videos.html">videos</a> links.</p>
+						<p>Pappy's celebrated our 11th Anniversary in June 2016. We appreciate your loyalty!  We offer Gift Cards in any amount and don't forget to ask about our Pappy's Rewards Program. See you at Pappy's!!</p>
+						<h3>Hours of Operation:</h3>
+							<ul>
+								<li>Monday - Thursday: 8:00am - 9:00pm</li>
+								<li>Friday: 8:00am - 11:00pm</li>
+								<li>Saturday: 8:00am - 10:00pm</li>
+							</ul>
+							<div>New Menu Items</div>
+					</div>
+				</div>
+			</nav>
+		</header>
+
+	</body>
+	<footer><?php include("mock-website/footer.php"); ?></footer>
+</html>
